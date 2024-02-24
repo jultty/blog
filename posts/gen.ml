7e960d57540e60670192747badef57ef12f58e7a
jultty <juno.t@tuta.io>
2024-02-24 13:40:43 -0300

    Refactor gen.ml with pipes

--- posts/gen.ml
+++ posts/gen.ml
@@ -1,12 +1,14 @@
 #!/usr/bin/env utop
 
-let pandoc_params = "--css ../../assets/style.css -s --to html5 " ^ 
+let pandoc_params = "--css ../../assets/style.css -s --to html5 " ^
   "--metadata pagetitle='basename' --highlight-style zenburn "
 
 let vert md = begin
-  if md <> "gen.ml" && md <> "html" then
-    ignore( Sys.command ("pandoc " ^ pandoc_params ^ md ^ " -o html/" ^ md ^ ".html ") )
+  let cmd = ("pandoc " ^ pandoc_params ^ md ^ " -o html/" ^ md ^ ".html ") in
+  ignore( Sys.command cmd )
 end ;;
 
-let contents = Array.to_list (Sys.readdir ".") in
-List.map vert contents ;;
+let contents = Sys.readdir "."
+  |> Array.to_list
+  |> List.filter (fun e -> e <> "gen.ml" && e <> "html")
+  |> List.map vert
