a722ab3a99db21f9128dc1dafe9dc93920448edb
jultty <juno.t@tuta.io>
2024-05-20 14:45:38 -0300

    Add footer text around data

--- assets/js/post-l10n.js
+++ assets/js/post-l10n.js
@@ -1,9 +1,13 @@
 const userLang = navigator.language || navigator.userLanguage
 let lang = userLang.includes('pt') ? 'pt' : 'en'
-console.log('loaded post-l10n.js', lang)
 
-if (lang != 'pt') {
+console.log('post-l10n.js loaded:', lang)
+
+if (lang == 'pt') {
   const footer_back = document.getElementById('footer-back-link')
-  console.log(footer_back)
-  footer_back.innerText = 'Back'
+  const author_pre = document.getElementById('author-pre-text')
+  const date_pre = document.getElementById('date-pre-text')
+  footer_back.innerText = 'Voltar'
+  author_pre.innerText = 'Postado por'
+  date_pre.innerText = 'em'
 }
