78cbd2f0f9318d86d4db0751480b53e895b2dbef
jutty <j@jutty.dev>
2026-05-05 13:22:47 -0300

    Fix missing root path argument in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ set -eu
 
 git_root=$(git rev-parse --show-toplevel)
 
-zola check --skip-external-links
+zola --root "$git_root" check --skip-external-links
 zola --root "$git_root" build
 "$git_root"/scripts/octothorpe-index.sh
